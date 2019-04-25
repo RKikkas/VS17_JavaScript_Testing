ffae39c38293784c60e139ffd66ccfad905a211c
RKikkas <roger.kikkas@gmail.com>
2019-04-25 20:05:19 +0300

    13. Another Spec

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -13,4 +13,14 @@ describe('Address Book', function() {
         // mille me lisasime
         expect(addressBook.getContact(0)).toBe(thisContact);
     });
+
+    it('should be able to delete a contact', function() {
+        var addressBook = new AddressBook(),
+            thisContact = new Contact();
+
+        addressBook.addContact(thisContact);
+        addressBook.deleteContact(0);
+
+        expect(addressBook.getContact(0)).not.toBe(thisContact);
+    })
 });
