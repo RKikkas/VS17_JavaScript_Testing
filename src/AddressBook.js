c22957ef039296e4f9afa03a6990332364e745b4
RKikkas <roger.kikkas@gmail.com>
2019-04-25 20:02:32 +0300

    12. Mike's Solution

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,3 +1,13 @@
 function AddressBook() {
-
+    this.contacts = [];
 }
+
+// Meetod mis lisab kontakti aadressiraamatusse
+AddressBook.prototype.addContact = function(contact) {
+    this.contacts.push(contact);
+};
+
+// Meetod, mis võtab kindla indeksiga elemendi kontaktide arrayst
+AddressBook.prototype.getContact = function(index) {
+    return this.contacts[index];
+};
